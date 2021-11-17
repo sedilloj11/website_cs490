d03ab9497fa74effe7e7962b0f67b486995a44d0
sedilloj11 <48217629+sedilloj11@users.noreply.github.com>
2021-11-17 14:05:01 -0800

    Update createTest.php

--- createTest.php
+++ createTest.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); //Requiring sesssion to enter
+session_start(); //Requiring session to enter
 
   include("dbConnect.php"); //requires file for connection to database
   include("functions.php"); //require file where functions are implemented
@@ -8,7 +8,35 @@ session_start(); //Requiring sesssion to enter
   
   //Check if the POST method was used
   if($_SERVER['REQUEST_METHOD'] == "POST"){
-  
+	  
+	$diff = $_POST['diff'];
+	$type = $_POST['type'];
+	
+	if(isset($_POST['search'])) {
+		if($_POST['diff'] != '') {
+			if($_POST['type'] != '') {
+				$query = "SELECT * FROM CS490Questions WHERE difficulty ='".$diff."' AND type='".$type."'";
+				$qResult = mysqli_query($db,$query);
+			}
+			else {
+				$query = "SELECT * FROM CS490Questions WHERE difficulty ='.$diff.'";
+				$qResult = mysqli_query($db,$query);
+			}
+		}
+		else if($_POST['type'] != '') {
+			$query = "SELECT * FROM CS490Questions WHERE type='%".$type."%'";
+			$qResult = mysqli_query($db,$query);
+		}
+		else {
+			$query = "SELECT * FROM CS490Questions";
+			$qResult = mysqli_query($db,$query);
+		}
+	}
+	else {
+		$query = "SELECT * FROM CS490Questions";
+		$qResult = mysqli_query($db,$query);
+	}  
+		
     //Getting all submitted scores from text boxes
     $allScores = [];
     $submittedScores = $_POST['scores'];
@@ -70,14 +98,33 @@ session_start(); //Requiring sesssion to enter
   <a href="questions.php">Questions</a> | <a href="welcomeAdmin.php">Home</a>
   <h1>Select Questions</h1><br>
   
+  
   <form method="post">
+  
+	<label for="diff">Filter Question Difficulty:</label><br>
+	<select name="diff" id="diff">
+		<option value="easy">Easy</option>
+		<option value="medium">Medium</option>
+		<option value="hard">Hard</option>
+	</select><br><br>
+	
+	<label for="type">Filter Question Type:</label><br>
+	<select name="type" id="type">
+		<option value="for">For</option>
+		<option value="while">While</option>
+		<option value="recursion">Recursion</option>
+		<option value="other">Other</option>
+	</select><br><br>
+	
+	<input type="submit" name="search" value="Filter"><br><br>
   
     <label for="testName">Test Name:</label><br>
     <input type="text" id="testName" name="testName"><br><br>
     <?php
+	
     //Retrieve all questions from the database
-    $query = "SELECT * FROM CS490Questions";
-    $qResult = mysqli_query($db,$query);
+    //$query = "SELECT * FROM CS490Questions";
+    //$qResult = mysqli_query($db,$query);
     $qCheck = mysqli_num_rows($qResult);//Will be set to the number of rows retrieved by the query
     
     if($qCheck > 0){
