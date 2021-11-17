973048646fc765fb040aacf07f591f257eb9d44c
sedilloj11 <48217629+sedilloj11@users.noreply.github.com>
2021-11-16 17:16:04 -0800

    Update grading.php

--- grading.php
+++ grading.php
@@ -1,4 +1,3 @@
-grading.php
 
 <?php
 session_start(); //Requiring sesssion to enter
@@ -10,23 +9,12 @@ session_start(); //Requiring sesssion to enter
   $dataOfUser = isLoggedIn($db);//Assign information of the student. can retrieve id
   
   
-  if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $st_ID = $dataOfUser['id']; //STUDENT ID
-
-    $testID = $_POST['selected_test'];
-  
-  
-    if(!empty($testID)){
-    // test is not valid
-    }
-
-
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
   <title>Blanck Test</title>
     </head>
 
@@ -37,30 +25,38 @@ session_start(); //Requiring sesssion to enter
     background-color: #b0aa8c;
   }
   </style>
-  <h1><?php echo $_POST["selected_exam"]?></h1><br>
+  <h1><?php echo $_POST["selected_test"]?></h1><br>
+  
+
   
  
   
     <?php
       //Retrieving from Database
-      $testID = $_POST["selected_exam"];
-      //echo $testName . "<br>";
-      $query = "SELECT * FROM CS490Answers WHERE unique_id='$testID'";
+      $testID = $_POST["selected_test"];
+      
+      $query = "SELECT * FROM CS490Answers WHERE unique_id = $testID";
       $qResult = mysqli_query($db,$query);
       $qCheck = mysqli_num_rows($qResult);
+      echo $qCheck;
+    
       while($row = mysqli_fetch_assoc($qResult)){
         
-        $answer = $row['answer']
+        $answer = $row['answer'];
+        $Qid = $row['question id'];
+        echo $row["answer"];
+        echo "<br>";
+        echo $Qid;
       //Questions pull from CS490Questions using Qid from ^^^ to get 
-        $questQuery = "SELECT * FROM CS490Questions Where qID = $row['question id']";
-        $qResult = mysqli_query($db,$questQuery);
-        $Q = $qresult->fetch_assoc();
+        $questQuery = "SELECT * FROM CS490Questions WHERE qID = $Qid";
+        $questResult = mysqli_query($db,$questQuery);
+        $Q = mysqli_fetch_assoc($questResult);
 
-        $question = $Q[1];//question
-        $c1 = $Q[2];//case1
-        $r1 = $Q[3];//result1
-        $c2 = $Q[4];//case2
-        $c3 = $Q[5];//result2
+        $question = $Q['question'];//question
+        $c1 = $Q['qCase1'];//case1
+        $r1 = $Q['qResult1'];//result1
+        $c2 = $Q['qCase2'];//case2
+        $c3 = $Q['qResult2'];//result2
 
         echo "<br>". $question ."|". $answer." <br>";
   
